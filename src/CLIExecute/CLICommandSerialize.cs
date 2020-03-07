508de0e5071b4c0d58d3c2ad320a47f1e7cc0de5
ignatandrei <ignatandrei@yahoo.com>
2020-03-07 12:01:07 +0200

    eliminate not used code

--- src/CLIExecute/CLICommandSerialize.cs
+++ src/CLIExecute/CLICommandSerialize.cs
@@ -8,18 +8,18 @@ namespace CLIExecute
 {
     public class CLICommandSerialize
     {
-        public static string Serialize(CLICommand_v1 v1)
-        {
-            var serializer = new SerializerBuilder().Build();
-            return serializer.Serialize(v1);
-        }
-        public static ICLICommand_v1 DeSerializeV1(string v1)
-        {
-            var deserializer = new DeserializerBuilder()
-                .WithNamingConvention(CamelCaseNamingConvention.Instance)
-                .Build();
-            return deserializer.Deserialize<CLICommand_v1>(v1);
-        }
+        //public static string Serialize(CLICommand_v1 v1)
+        //{
+        //    var serializer = new SerializerBuilder().Build();
+        //    return serializer.Serialize(v1);
+        //}
+        //public static ICLICommand_v1 DeSerializeV1(string v1)
+        //{
+        //    var deserializer = new DeserializerBuilder()
+        //        .WithNamingConvention(CamelCaseNamingConvention.Instance)
+        //        .Build();
+        //    return deserializer.Deserialize<CLICommand_v1>(v1);
+        //}
         public static string Serialize(object o)
         {
             var serializer = new SerializerBuilder().Build();
