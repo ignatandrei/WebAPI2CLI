d24da8c71051e346278a950431c2678d34ce4673
ignatandrei <ignatandrei@yahoo.com>
2020-03-07 10:34:55 +0200

    no ssl ererors

--- src/TestWebAPISite/Program.cs
+++ src/TestWebAPISite/Program.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -13,6 +16,15 @@ namespace TestWebAPISite
     {
         public static void Main(string[] args)
         {
+            ServicePointManager.ServerCertificateValidationCallback =
+            delegate (
+                object s,
+                X509Certificate certificate,
+                X509Chain chain,
+                SslPolicyErrors sslPolicyErrors
+            ) {
+                return true;
+            };
             CreateHostBuilder(args).Build().Run();
         }
 
