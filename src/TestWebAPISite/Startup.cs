e177b18631f951fc8e52d6d29b5344d81d42f1a2
ignatandrei <ignatandrei@yahoo.com>
2020-03-10 00:12:10 +0200

    publish tests as artifacts in AzureDevOps

--- src/TestWebAPISite/Startup.cs
+++ src/TestWebAPISite/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace TestWebAPISite
 {
@@ -28,11 +29,29 @@ namespace TestWebAPISite
         {
             services.AddCLI();
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { 
+                    Title = "WebAPI2CLI", 
+                    Version = "v1",
+                    Contact=new OpenApiContact(){
+                    Email=   "ignatandrei@yahoo.com",
+                    Url =new Uri( "http://msprogrammer.serviciipeweb.ro/category/WEBAPI2CLI/"),
+                    Name="Andrei Ignat"
+                    },
+                    Description=" please run the project with TestWebAPISite.exe --CLI_Enabled=1 --CLI_COMMANDS=\"GetMathId_Http,WeatherGet\" "
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI2CLI");
+            });
             app.UseCLI();
             if (env.IsDevelopment())
             {
