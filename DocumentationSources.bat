82eac74c64e57f8def41c73e4f153048b09b4b65
ignatandrei <ignatandrei@yahoo.com>
2020-03-16 21:29:11 +0200

    packages list

--- DocumentationSources.bat
+++ DocumentationSources.bat
@@ -1,7 +1,7 @@
 cd externals
 cd docfx
-rmdir ..\..\docs\sitedocs
-mkdir ..\..\docs\sitedocs
+rmdir ..\..\docs\sitedocs /s /q
+mkdir ..\..\docs\sitedocs 
 docfx Documentation\docfx.json -o ..\..\docs\DocumentationAPIcd 
 xcopy ..\..\docs\DocumentationAPIcd\_site ..\..\docs\sitedocs /S /Y
 rmdir ..\..\docs\DocumentationAPIcd /s /q
---
+++ docs/packageListHeader.md
@@ -0,0 +1,1 @@
+# Referenced packages
